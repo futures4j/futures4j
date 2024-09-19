d66f7c74d6102611706750215a8431972b4049ef
sebthom <sebthom@users.noreply.github.com>
2024-09-19 23:34:44 +0200

    docs: extend javadoc

--- src/main/java/io/github/futures4j/CompletionState.java
+++ src/main/java/io/github/futures4j/CompletionState.java
@@ -12,22 +12,50 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Describes the completion state of a {@link Future}
+ * Represents the possible completion states of a {@link Future}, offering a streamlined
+ * method to check whether a task is still pending, has completed successfully, or has
+ * terminated due to cancellation or other exceptional conditions.
+ * <p>
+ * This utility is compatible with different implementations of {@link Future}, such as {@link CompletableFuture}
+ * and {@link ForkJoinTask}, and provides a consistent way to analyze their states.
+ * </p>
  *
  * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
  */
 public enum CompletionState {
+
+   /**
+    * Indicates that the task hasn't started or is still in progress.
+    */
    INCOMPLETE,
 
    /**
-    * Completed normally/successfully.
+    * Indicates that the task has completed successfully without any errors.
     */
    COMPLETED,
 
+   /**
+    * Indicates that the task was cancelled before completion.
+    */
    CANCELLED,
 
+   /**
+    * Indicates that the task failed due to an exception, not related to cancellation.
+    */
    FAILED;
 
+   /**
+    * Determines the {@link CompletionState} of the provided {@link Future} based on its current status.
+    * <ul>
+    * <li>If the future is cancelled, it returns {@link #CANCELLED}.</li>
+    * <li>If the future completed exceptionally (non-cancellation), it returns {@link #FAILED}.</li>
+    * <li>If the future completed successfully, it returns {@link #COMPLETED}.</li>
+    * <li>If the future is still in progress or the task has not started, it returns {@link #INCOMPLETE}.</li>
+    * </ul>
+    *
+    * @param future the {@link Future} whose completion state is to be determined
+    * @return the {@link CompletionState} representing the current state of the future
+    */
    public static CompletionState of(final Future<?> future) {
       if (future.isDone()) {
          if (future.isCancelled())
